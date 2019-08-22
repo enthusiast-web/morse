0949b29eafb4c579d11bde6a49a9909870a74631
unknown <samuellpvt@gmail.com>
2019-08-22 10:34:14 -0300

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn morse.morse.wsgi
+python manage.py migrate
--- morse/morse/settings.py
+++ morse/morse/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'morse.morse.wsgi.application'
+WSGI_APPLICATION = 'morse.wsgi.application'
 
 
 # Database
