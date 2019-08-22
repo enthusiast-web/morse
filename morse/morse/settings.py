9d2dc4f04f8705f6b84e3c0d7dbeda3eb283fe28
unknown <samuellpvt@gmail.com>
2019-08-22 10:38:07 -0300

    heroku

--- morse/morse/settings.py
+++ morse/morse/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api.apps.ApiConfig',
-    'frontend.apps.FrontendConfig',
+    # 'api.apps.ApiConfig',
+    # 'frontend.apps.FrontendConfig',,
+    'morse'
    
 ]
 
