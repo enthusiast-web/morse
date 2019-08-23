99b9d9d91597fad8d8484303d51109e0931d6de8
unknown <samuellpvt@gmail.com>
2019-08-23 14:24:42 -0300

    tradutor

--- frontend/src/components/langToMorse.js
+++ frontend/src/components/langToMorse.js
@@ -7,6 +7,8 @@ import {
   createCTX
 } from "../actions/morseActions";
 import Icon from "@material-ui/core/Icon";
+
+import Speech from "speak-tts";
 class langToMorse extends Component {
   state = {
     texto: "",
@@ -15,7 +17,8 @@ class langToMorse extends Component {
     lista_normal: [],
     morse: "",
     morseTraduzido: [],
-    display: true
+    display: true,
+    displayLangToMorse: true
   };
 
   componentDidMount() {
@@ -27,15 +30,15 @@ class langToMorse extends Component {
   componentWillUnmount() {
     window.removeEventListener("resize", this.display);
     window.removeEventListener("resize", this.update);
-    
   }
-  // isso funciona para força o rerender :( 
+  // isso funciona para força o rerender :(
   update = () => {
     this.setState({ state: this.state });
   };
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-    this.morseTolang();
+
+    console.log(this.state.morse, this.state.texto);
     // esta retorando uma array pq dois metodos utilizam esse return
     // e como está direto no render nao pode mexer no state
   };
@@ -43,18 +46,18 @@ class langToMorse extends Component {
     var lista = [];
     var lista_normal = [];
     [...this.state.texto].map((obj, ind) => {
-      if (obj === " ") {
+      if (obj === " " && this.state.texto[ind + 1] !== " ") {
         lista_normal = [...lista_normal, " / "];
-        lista = [...lista, ["  /  "]];
+        lista = [...lista, [" "]];
       }
       for (var i in this.props.morse.letras) {
         if (obj === i) {
           lista_normal = [...lista_normal, " ", this.props.morse.letras[i]];
-          lista = [...lista, ["  " + this.props.morse.letras[i], " " + i]];
+          lista = [...lista, [" " + this.props.morse.letras[i], " " + i]];
         }
       }
     });
-    // console.log(lista_normal);
+
     return [lista_normal, lista];
   };
 
@@ -62,12 +65,12 @@ class langToMorse extends Component {
     var ms = this.state.morse.split(" ");
     var toReturn = [];
 
-    ms.map(obj => {
+    ms.map((obj, ind) => {
       for (var i in this.props.morse.letras) {
         if (obj === this.props.morse.letras[i]) {
           toReturn.push([i, obj]);
         } else {
-          if (obj === "") {
+          if (obj === "" || (obj.includes("/") && ms[ind + 1] !== "")) {
             toReturn.push([" ", ""]);
             break;
           }
@@ -129,7 +132,48 @@ class langToMorse extends Component {
       }, total);
     }
   };
+  translate = value => {
+    var trans = "";
+    if (!value) {
+      var ms = this.state.morse.split(" ");
+      trans = ms.map(obj => {
+        for (var i in this.props.morse.letras) {
+          if (obj === this.props.morse.letras[i]) {
+            return trans.concat(i);
+          } else {
+            if (obj === "" || obj.includes("/")) {
+              return trans.concat("  ");
+            }
+          }
+        }
+      });
+      trans = trans.join("");
+    } else {
+      trans = value;
+    }
 
+    const speech = new Speech();
+    speech.setLanguage("pt-BR");
+    speech
+      .init()
+      .then(data => {
+        // The "data" object contains the list of available voices and the voice synthesis params
+        console.log("Speech is ready, voices are available", data);
+      })
+      .catch(e => {
+        console.error("An error occured while initializing : ", e);
+      });
+    speech
+      .speak({
+        text: trans
+      })
+      .then(() => {
+        console.log("Success !");
+      })
+      .catch(e => {
+        console.error("An error occurred :", e);
+      });
+  };
   getColor = ind => {
     var letters = "0123456789ABCDEF";
     var color = "#";
@@ -142,20 +186,33 @@ class langToMorse extends Component {
   // caso contrario mostra o botao de switch
   // functional style
   display = () => {
-    if (window.innerWidth < 999) {
-      if (this.state.display) {
-        return ["none", "block", "linguagem", "morse"];
-      } else {
-        return ["block", "none", "morse", "linguagem "];
-      }
-    } else return ["block", "block", "", ""];
+    if (this.state.display) {
+      return ["none", "block", "linguagem", "morse"];
+    } else {
+      return ["block", "none", "morse", "linguagem "];
+    }
+  };
+  displayLangToMorse = () => {
+    if (this.state.displayLangToMorse) {
+      return ["none", "block", "linguagem", "morse"];
+    } else {
+      return ["block", "none", "morse", "linguagem "];
+    }
+  };
+  displayMorseToLang = () => {
+    if (this.state.displayMorseToLang) {
+      return ["none", "block", "linguagem", "morse"];
+    } else {
+      return ["block", "none", "morse", "linguagem "];
+    }
   };
   render() {
     return (
       <div className="container m-0">
+        {/* <Icon>volume_up</Icon> */}
         <div className="row">
-          <div className="d-xl-none d-lg-none container   m-0 p-0">
-            <div className="row  no-gutters">
+          <div className="container   m-0 p-0">
+            <div className="row  no-gutters" style={{ maxWidth: 500 }}>
               <div className="col-sm">{this.display()[2]}</div>
 
               <Icon
@@ -163,6 +220,16 @@ class langToMorse extends Component {
                 fontSize="large"
                 onClick={() => {
                   this.setState({ display: !this.state.display });
+                  this.setState({
+                    texto: this.morseTolang()
+                      .map(obj => obj[0])
+                      .join("")
+                      .replace(/^\s+/g, ""),
+                    morse: this.ChangeLista()[1]
+                      .map(obj => obj[0])
+                      .join("")
+                      .replace(/^\s+/g, "")
+                  });
                 }}
               >
                 compare_arrows
@@ -183,10 +250,32 @@ class langToMorse extends Component {
               placeholder="digite aqui em letras"
               type="text"
             />
-            <div>
+            <div className="d-flex">
+              <Icon
+                fontSize="large"
+                onClick={() => {
+                  this.translate(this.state.texto);
+                }}
+              >
+                volume_up
+              </Icon>
               <Icon fontSize="large" onClick={() => this.play()}>
                 play_arrow
               </Icon>
+
+              <input
+                style={{ marginTop: "auto", marginBottom: "auto" }}
+                type="checkbox"
+                value={this.state.displayLangToMorse}
+                onChange={() =>
+                  this.setState({
+                    displayLangToMorse: !this.state.displayLangToMorse
+                  })
+                }
+              />
+              <label style={{ marginTop: "auto", marginBottom: "auto" }}>
+                somente morse
+              </label>
             </div>
             <div
               className="container"
@@ -199,49 +288,63 @@ class langToMorse extends Component {
               }}
             >
               <div className="row no-gutters ">
+                <div
+                  style={{
+                    display: this.displayLangToMorse()[0],
+                    whiteSpace: "normal",
+                    fontSize: "20px",
+                    textAlign: "center",
+                    marginBottom: 0,
+                    padding: 0,
+                    margin: 0
+                  }}
+                >
+                  {this.ChangeLista()[0].map((obj, ind) => obj)}
+                </div>
+
                 {this.ChangeLista()[1].map((obj, ind) => {
-                  if (obj !== "") {
-                    return (
-                      <div
-                        className="col-1"
-                        key={ind}
+                  return (
+                    <div
+                      className="col-1"
+                      key={ind}
+                      style={{
+                        display: this.displayLangToMorse()[1],
+                        minWidth: "15px",
+                        height: "auto",
+                        margin: "0px 0px 0px 5px",
+                        fontSize: "20px",
+                        color: this.getColor(ind),
+                        paddingRight: 10,
+                        margin: 0,
+                        borderBottom: "1px solid black"
+                      }}
+                    >
+                      <p
                         style={{
-                          height: "auto",
-                          margin: "0px 0px 0px 5px",
-                          fontSize: "20px",
-                          color: this.getColor(ind),
-                          paddingRight: 10,
-                          margin: 0,
-                          borderBottom: "1px solid black"
+                          whiteSpace: "normal",
+                          width: 40,
+                          textAlign: "center",
+                          marginBottom: 0,
+                          padding: 0,
+                          margin: 0
                         }}
                       >
-                        <p
-                          style={{
-                            whiteSpace: "normal",
-                            width: 40,
-                            textAlign: "center",
-                            marginBottom: 0,
-                            padding: 0,
-                            margin: 0
-                          }}
-                        >
-                          {obj[1]}
-                        </p>
-                        <p
-                          style={{
-                            whiteSpace: "normal",
-                            width: 40,
-                            textAlign: "center",
-                            marginBottom: 0,
-                            padding: 0,
-                            margin: 0
-                          }}
-                        >
-                          {obj[0]}
-                        </p>
-                      </div>
-                    );
-                  }
+                        {obj[1]}
+                      </p>
+                      <p
+                        style={{
+                          whiteSpace: "normal",
+                          width: 40,
+                          textAlign: "center",
+                          marginBottom: 0,
+                          padding: 0,
+                          margin: 0
+                        }}
+                      >
+                        {obj[0]}
+                      </p>
+                    </div>
+                  );
                 })}
               </div>
             </div>
@@ -259,55 +362,94 @@ class langToMorse extends Component {
               placeholder="digite aqui em codigo morse ,ex: ..- ..-"
               type="text"
             />
-            <div className="m-0 p-0">
-              <Icon fontSize="large" onClick={() => this.play("arg")}>
+            <div className="m-0 p-0 d-flex">
+              <Icon
+                fontSize="large"
+                onClick={() => {
+                  this.translate();
+                }}
+              >
+                volume_up
+              </Icon>
+              <Icon
+                fontSize="large"
+                onClick={() => {
+                  this.play("arg");
+                }}
+              >
                 play_arrow
               </Icon>
+              <input
+                style={{ marginTop: "auto", marginBottom: "auto" }}
+                type="checkbox"
+                value={this.state.displayMorseToLang}
+                onChange={() =>
+                  this.setState({
+                    displayMorseToLang: !this.state.displayMorseToLang
+                  })
+                }
+              />
+              <label style={{ marginTop: "auto", marginBottom: "auto" }}>
+                somente letras
+              </label>
             </div>
             <div className="container m-0 p-0" style={{ maxWidth: 500 }}>
               <div className="row no-gutters ">
+                <div
+                  style={{
+                    display: this.displayMorseToLang()[1],
+                    whiteSpace: "normal",
+                    fontSize: "20px",
+                    textAlign: "center",
+                    marginBottom: 0,
+                    padding: 0,
+                    margin: 0
+                  }}
+                >
+                  {this.morseTolang().map((obj, ind) => obj[0])}
+                </div>
                 {this.morseTolang().map((obj, ind) => {
-                  if (obj[1] !== "") {
-                    return (
-                      <div
-                        key={ind}
+                  return (
+                    <div
+                      key={ind}
+                      style={{
+                        display: this.displayMorseToLang()[0],
+                        minWidth: 15,
+                        margin: "0px 0px 0px 10px",
+                        fontSize: "20px",
+                        color: this.getColor(ind),
+                        borderBottom: "1px solid black",
+                        marginBottom: 0,
+                        paddingRight: 10,
+                        margin: 0
+                      }}
+                    >
+                      <p
                         style={{
-                          margin: "0px 0px 0px 10px",
-                          fontSize: "20px",
-                          color: this.getColor(ind),
-                          borderBottom: "1px solid black",
+                          whiteSpace: "normal",
+                          width: 40,
+                          textAlign: "center",
                           marginBottom: 0,
-                          paddingRight: 10,
+                          padding: 0,
                           margin: 0
                         }}
                       >
-                        <p
-                          style={{
-                            whiteSpace: "normal",
-                            width: 40,
-                            textAlign: "center",
-                            marginBottom: 0,
-                            padding: 0,
-                            margin: 0
-                          }}
-                        >
-                          {obj[1]}
-                        </p>
-                        <p
-                          style={{
-                            whiteSpace: "normal",
-                            width: 40,
-                            textAlign: "center",
-                            marginBottom: 0,
-                            padding: 0,
-                            margin: 0
-                          }}
-                        >
-                          {obj[0]}
-                        </p>
-                      </div>
-                    );
-                  }
+                        {obj[1]}
+                      </p>
+                      <p
+                        style={{
+                          whiteSpace: "normal",
+                          width: 40,
+                          textAlign: "center",
+                          marginBottom: 0,
+                          padding: 0,
+                          margin: 0
+                        }}
+                      >
+                        {obj[0]}
+                      </p>
+                    </div>
+                  );
                 })}
               </div>
             </div>
